27a393606bd36928d94484acc0a318f03dd6d6ed
Legaz <worldoflegaz@icloud.com>
2025-07-22 14:31:10 +0900

    Creating Redux Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+const store = configureStore({
+  reducer: {},
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+    </Provider>
+  </StrictMode>
+);
