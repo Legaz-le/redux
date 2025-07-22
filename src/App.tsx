7b7277f354f89a9160925808780881444e63c943
Legaz <worldoflegaz@icloud.com>
2025-07-22 14:10:35 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <>
+     <Profile />
+     <Login />
+    </>
+  )
+}
+
+export default App
