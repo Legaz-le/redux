ce6f260b8bf7a45ec5103c52a4eed983ac84fac4
Legaz <worldoflegaz@icloud.com>
2025-07-22 15:59:56 +0900

    logout reducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,24 @@
 import { useDispatch } from "react-redux";
-import { login } from "../features/user";
+import { login, logout } from "../features/user";
 
 const Login = () => {
   const dispatch = useDispatch();
   return (
     <div>
-      <button onClick={()=> {dispatch(login({ name: "Fer", age: 50, email: "Fer@gmai.com" }))}}>Login</button>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Fer", age: 50, email: "Fer@gmai.com" }));
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
